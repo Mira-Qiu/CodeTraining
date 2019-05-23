14c2ee81db1f18457013709dd7704ba54f61ffb0
Mira Qiu <qmm0607@gmail.com>
2019-05-22 23:27:05 -0400

    Update PQTest.java

--- PriorityQueue/PQTest.java
+++ PriorityQueue/PQTest.java
@@ -1,1 +1,5 @@
 Queue<Interger> pq = new PriorityQueue<>(l1, new comparator<Integer>(){
+  public int compare compare(Integer i1, Integer i2( {
+    return i2-i1;
+  }
+                                                    });
