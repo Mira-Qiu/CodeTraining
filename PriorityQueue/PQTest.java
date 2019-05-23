4f9f92f39d24a689cf415d20b3267ea82979d554
Mira Qiu <qmm0607@gmail.com>
2019-05-22 23:46:50 -0400

    Update PQTest.java

--- PriorityQueue/PQTest.java
+++ PriorityQueue/PQTest.java
@@ -1,5 +1,8 @@
+
+//自己指定比较器进行排序
+
 Queue<Interger> pq = new PriorityQueue<>(l1, new comparator<Integer>(){
-  public int compare compare(Integer i1, Integer i2( {
-    return i2-i1;
-  }
+                public int compare compare(Integer i1, Integer i2( {
+                      return i2-i1;
+                                                    }
                                                     });
