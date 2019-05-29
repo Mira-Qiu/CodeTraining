d17260ec345f3fc78a2094dcd77060483a2f59f5
Mira Qiu <qmm0607@gmail.com>
2019-05-28 23:30:19 -0400

    Update DistanceK.java

--- BinaryTree/DistanceK.java
+++ BinaryTree/DistanceK.java
@@ -72,3 +72,55 @@ import javax.swing.tree.TreeNode;
          }
      }
  }
+
+
+//Method 2: Percolate distance
+
+import javax.swing.tree.TreeNode;
+
+class Demo {
+    List<Integer> ans;
+    TreeNode target;
+    int K;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K){
+        ans = new LinkedList<>();
+        dfs(root);
+        return ans;
+    }
+
+    //dfs return the distance from the node to the target
+    private int dfs(TreeNode node){
+        if (node == null){
+            return -1;
+        } else if (node == target){
+            subtree_add(node, 0);
+            return 1;
+        } else {
+            int L = dfs(node.left), R = dfs(node.right);
+            if (L != -1){
+                if (L == K)
+                    ans.add(node.val);
+                subtree_add(node.right, L + 1);
+                return L + 1;
+            } else if ( R != -1){
+                if (R == K)
+                    ans.add(node.val);
+                subtree_add(node.right, R + 1);
+                return R + 1;
+            } else {
+                return =1;
+            }
+            
+        }
+    }
+    // subtree_add : add the nodes int the subtree rooted at the node distance K - distance from the given node
+    private void subtree_add(TreeNode node, int dist){
+        if (node == null) return;
+        if (dist == K){
+            ans.add(node.val);
+        } else {
+            subtree_add(node.left, dist + 1);
+            subtree_add(node.right, dist+ 1);
+        }
+    }
+}
