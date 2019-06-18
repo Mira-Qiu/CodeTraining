2618711b6e45c12f0c907d0bdd525f0ea12d126c
Mira Qiu <qmm0607@gmail.com>
2019-06-18 18:26:14 -0400

    Update recursion.java

--- Recursion/recursion.java
+++ Recursion/recursion.java
@@ -2,3 +2,19 @@
 - base case
 - recurrence relation
 
+// print a string in reverse order.
+
+first, we can define the desired functions as *printReverse(str[0,....[n-1])*,
+  step 1 : printReverse(str[0,...[n-1]): print printReverse(str[1,...[n-1]) in reverse order;
+  step 2 : print(str[0]): print the first character in the string.
+class solution{
+ private void printReverse(char[] str){
+  helper(0, str); 
+ }
+  private void helper(int index, char[] str){
+   if (str == nul || index >= str.length) return;
+    helper(index + 1, str);
+    System.out.println(str[index]);
+  }
+}
+                                                             
